86a9faeeebdb49afdab2d7eab9e03cc5c7214401
tiesen243 <ttien56906@gmail.com>
2025-05-05 23:19:48 +0700

    fix: build error

--- apps/nextjs/app/page.tsx
+++ apps/nextjs/app/page.tsx
@@ -3,6 +3,8 @@ import { Suspense } from 'react'
 import { getQueryClient, HydrateClient, trpc } from '@/lib/trpc/server'
 import { CreatePostForm, PostCardSkeleton, PostList } from './page.client'
 
+export const dynamic = 'force-dynamic'
+
 export default function HomePage() {
   const queryClient = getQueryClient()
   void queryClient.prefetchQuery(trpc.post.all.queryOptions())
